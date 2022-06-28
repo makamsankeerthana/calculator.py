e787c7631d3a8ae0dc06f48a4ebd8be18037a777
makamsankeerthana <106650601+makamsankeerthana@users.noreply.github.com>
2022-06-29 01:06:33 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,3 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
 >>> import math
 print("select an operation to perform: (1 - 6 ")
 print("1.ADD")
